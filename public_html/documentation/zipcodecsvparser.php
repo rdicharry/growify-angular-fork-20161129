40cae60baaf6726e746439e504669a13ea5fef1f
DannyEaton <danieleaton747@gmail.com>
2016-10-31 13:07:10 -0600

    Added zipcodecsvparse
    Added zipcodecsvparser file that parses through zip_code_database and returns NM zipcodes with other info

---
+++ public_html/documentation/zipcodecsvparser.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Users: Daniel Eaton, Rebecca, Greg, Ana
+ * Date: 10/29/2016
+ * Time: 9:34 PM
+ */
+
+
+$file = file_get_contents('zip_code_database.csv');
+$csv = str_getcsv($file);
+for($x = 0; $x < (count($csv) - 3); $x += 3){
+	if(substr($csv[$x],0,2) == "NM"){
+		echo('Zip Code: ');
+		print_r($csv[$x] . "<br />");
+		print_r("City: ". $csv[$x+1]."<br/>");
+		print_r("Additional Areas Covered: " . $csv[$x+2] . "<br/>");
+	}
+}
+
+?>
