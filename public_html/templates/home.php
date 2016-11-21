a00e55aa109717d4d1a8ff3642da067b160db5b2
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-21 10:35:26 -0700

    added home-component, and basic templates (home and growify-app) updated with our app name (growify)
    added .htaccess
    
    https://app.asana.com/0/201954860780444/213184219448280

---
+++ public_html/templates/home.php
@@ -0,0 +1,9 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to Growify!</h1>
+			<p class="lead">This is an app for supplying growing information for home gardeners!</p>
+			<!--<a routerLink="/weather">Weather</a>-->
+		</div>
+	</div>
+</section>
