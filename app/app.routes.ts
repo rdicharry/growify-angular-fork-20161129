bbdcf88bf2e8fdb08b232d02c0ceefdd83700a7b
Ana Vela <avela7@cnm.edu>
2016-11-22 13:48:32 -0700

    created component files https://app.asana.com/0/201954860780444/213184219448283

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,6 +2,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {LoginComponent} from "./components/login-component";
 import {SignupComponent} from "./components/signup-component";
+import {GardenComponent} from "./components/garden-component";
+import {PlantsComponent} from "./components/plants-component";
+import {SettingsComponent} from "./components/settings-component";
+import {SignoutComponent} from "./components/signout-component";
 
 
 export const allAppComponents = [ HomeComponent, LoginComponent, SignupComponent];
@@ -11,7 +15,13 @@ export const routes: Routes = [
 	// the paths should go from most specific to least specific.
 	{path: "", component: HomeComponent},
 	{path: "login", component: LoginComponent},
-	{path: "signup", component: SignupComponent}
+	{path: "signup", component: SignupComponent},
+	{path: "garden", component: GardenComponent},
+	{path: "plants", component: PlantsComponent},
+	{path: "settings", component: SettingsComponent},
+	{path: "signout", component: SignoutComponent},
+
+
 ];
 
 export const appRoutingProviders: any[] = [];
