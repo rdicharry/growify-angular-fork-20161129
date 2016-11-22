62656682d8923bb34d8a025cfb8b8836ea41658e
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-22 10:45:35 -0700

    restructure Plant and mySQL statements to better fit available plant data

---
+++ php/util/plantDataImport.php
@@ -0,0 +1,22 @@
+<?php
+
+//
+$usdaHardinessZones = [
+
+];
+
+$usdaHardinessToNMSUAreas = [
+
+];
+
+
+// get min temps -
+// if hardiness data available get from there
+
+// if plant is "frost tender" then set to 32F (esp. if this is higher than hardiness zone temp
+
+// if nothing specified, set to 32F
+
+
+// plant description - take from plant uses, uses notes, cultivation details, propagation, author, references
+
