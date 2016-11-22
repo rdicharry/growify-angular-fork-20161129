c2a1c52873bf7f0e533094a5c3e8b421997a33a8
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-22 13:11:12 -0700

    Plant Hardiness cross-reference
    https://app.asana.com/0/201954860780444/213184219448259
    https://app.asana.com/0/201954860780444/213184219448261

--- php/util/plantDataImport.php
+++ php/util/plantDataImport.php
@@ -1,14 +1,43 @@
 <?php
 
-//
+// minimum temp (degrees F) for USDA plant hardiness zones in NM
+// source: http://planthardiness.ars.usda.gov/
 $usdaHardinessZones = [
+	"4a" => -30.0,
+	"4b" => -25.0,
+	"5a" => -20.0,
+	"5b" => -15.0,
+	"6a" => -10.0,
+	"6b" => -5.0,
+	"7a" => 0.0,
+	"7b" => 5.0,
+	"8a" => 10.0,
+	"8b" => 15.0,
+	"9a" => 20.0,
+	"9b" => 25.0
 
 ];
+
+// cross-reference USDA plant hardiness (min winter temp) to NMSU planting areas
+// source http://aces.nmsu.edu/pubs/_circulars/CR457B.pdf
 
 $usdaHardinessToNMSUAreas = [
+	"4a" => 3,
+	"4b" => 3,
+	"5a" => 3,
+	"5b" => 3,
+	"6a" => 3,
+	"6b" => 2,
+	"7a" => 2,
+	"7b" => 1,
+	"8a" => 1,
+	"8b" => 1,
+	"9a" => 1,
+	"9b" => 1
 
 ];
 
+// iterate over PlantsForAFuture data and add to Plant table.
 
 // get min temps -
 // if hardiness data available get from there
@@ -20,3 +49,8 @@ $usdaHardinessToNMSUAreas = [
 
 // plant description - take from plant uses, uses notes, cultivation details, propagation, author, references
 
+
+// iterate over NMSU Vegetable Data and add to Plant table (remember to check if an entry already exists for a given Plant Name.
+
+
+// Add herb data?
