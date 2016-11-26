4667129a9ccd3e2f4c2701bdebf0f9fbb40347c4
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-26 07:10:35 -0700

    iterating over plants plantAreaImport.php
    https://app.asana.com/0/201954860780444/213184219448261

--- php/util/plantAreaImport.php
+++ php/util/plantAreaImport.php
@@ -24,6 +24,10 @@ require_once(dirname(__DIR__) . "/classes/autoload.php");
 $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/growify.ini");
 
 function importPlantingDates(\PDO $pdo){
+	global $nmsuArea1ToUSDAHardiness;
+	global $nmsuArea2ToUSDAHardiness;
+	global $nmsuArea3ToUSDAHardiness;
+
 
 	// for each plant table entry (plant variety) there are three growing zones
 	// for each growing zone there are several USDA Hardiness zones - need to create a table entry for each one
@@ -33,12 +37,39 @@ function importPlantingDates(\PDO $pdo){
 		while(($dataCSV = fgetcsv($handle, 0, ",", "\"")) !== FALSE) { // set length to zero for unlimited line length php > 5.1
 
 			$plantName = $dataCSV[0];
-			$plantArea1Dates = $dataCSV[];
-			$plantArea1Dates = $dataCSV[];
-			$plantArea1Dates = $dataCSV[];
+			$plantVariety = $dataCSV[1];
 
 			// get plant ID from Plant table via PDO
+			$query = "SELECT plantId FROM plant WHERE plantName = :plantName AND plantVariety = :plantVariety";
+			$statement = $pdo->prepare($query);
+			$parameters = ["plantName" => $plantName, "plantVariety" => $plantVariety];
+			$statement->execute($parameters);
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
 
+			$row = $statement->fetch();
+			if($row === false){
+				throw(new \PDOException("did not find Plant entry for plantName: ".$plantName." and plantVariety: ".$plantVariety));
+			}
+			$plantId = $row["plantId"];
+
+
+			// parse and unwrap planting dates ToDO
+			$plantArea1Dates = $dataCSV[3]; // TODO strip whitespace at beg & end (trim?)
+			$plantArea1Dates = $dataCSV[4]; // TODO
+			$plantArea1Dates = $dataCSV[5];
+
+			for($i=0; $i<count($nmsuArea1ToUSDAHardiness); $i++){
+				$usdaArea = $nmsuArea1ToUSDAHardiness[$i];
+				$plantArea = new PlantArea(); // TODO insert
+			}
+			for($i=0; $i<count($nmsuArea2ToUSDAHardiness); $i++){
+				$usdaArea = $nmsuArea2ToUSDAHardiness[$i];
+				$plantArea = new PlantArea(); // TODO insert
+			}
+			for($i=0; $i<count($nmsuArea3ToUSDAHardiness); $i++){
+				$usdaArea = $nmsuArea3ToUSDAHardiness[$i];
+				$plantArea = new PlantArea(); // TODO insert
+			}
 
 		} // end while data CSV
 	}// end if handle
