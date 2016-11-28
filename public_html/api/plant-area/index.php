86b090d2b820e6d16a3e451de22946a26096b555
Ana Vela <avela7@cnm.edu>
2016-11-28 10:32:44 -0700

    updated GET method for plant area API https://app.asana.com/0/201562232002793/213231162308857

--- public_html/api/plant-area/index.php
+++ public_html/api/plant-area/index.php
@@ -36,35 +36,37 @@ try {
 	$plantAreaPlantId = filter_input(INPUT_GET, "plantAreaPlantId", FILTER_VALIDATE_INT);
 	$plantAreaNumber = filter_input(INPUT_GET, "plantAreaNumber", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-	// ensure $plantAreaId is valid
-	if($plantAreaId < 0) {
-		throw(new InvalidArgumentException ("plant area Id cannot be negative", 405));
-	}
+
 
 // Handle GET request
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
 
-	// If id is present, get the plant area for that id
-	if(empty($plantAreaId) === false) {
-		$plantArea = PlantArea::getPlantAreaByPlantAreaId($pdo, $plantAreaId);
-		if($plantArea !== null) {
-			$reply->data = $plantArea;
-			//Here, we store the retreived PlantArea in the $reply->data state variable.
+		// If id is present, get the plant area for that id
+		if(empty($plantAreaId) === false) {
+			// ensure $plantAreaId is valid
+			if($plantAreaId < 0) {
+				throw(new InvalidArgumentException ("plant area Id cannot be negative", 405));
+			}
+			$plantArea = PlantArea::getPlantAreaByPlantAreaId($pdo, $plantAreaId);
+			if($plantArea !== null) {
+				$reply->data = $plantArea;
+				//Here, we store the retrieved PlantArea in the $reply->data state variable.
+			}
 		}
-
-	} else {
-		$plantAreas = PlantArea::getAllPlantAreas($pdo);
-		if($plantAreas !== null) {
-			$reply->data = $plantAreas;
-		}
-	}
-} else {
-		throw(new InvalidArgumentException("Invalid HTTP method request"));
-	}
 
 // if plant id and plant area number are present get the plant area for that combination -- (need help with this)
+		if (empty($plantId) === false && empty($plantAreaNumber) === false) {
+			$plantArea = PlantArea::getPlantAreaByPlantIdAndAreaNumber($pdo, $plantId, $plantAreaNumber);
+			if($plantArea !== null) {
+				$reply->data = $plantArea;
+			}
+		}
+
+	}else {
+		throw(new InvalidArgumentException("Invalid HTTP method request"));
+	}
 
 
 } catch (Exception $exception){
@@ -79,7 +81,7 @@ header("Content-type: application/json");
 if($reply->data === null){
 	unset($reply->data);
 }
-	echo json_encode($reply);
+echo json_encode($reply);
 
 
 
