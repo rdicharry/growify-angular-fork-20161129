0e2d2a4ec2ade3828c34d52cba361916d25e8b17
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-28 16:52:04 -0700

    Update weather class and weather-components on angular end.
    https://app.asana.com/0/201954860780444/219309980456046

---
+++ public_html/templates/weather.php
@@ -0,0 +1,14 @@
+<section class="weather-display">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<ul>
+					<li>Min Temp: {{dailyWeather.temperatureMin}} degF</li>
+					<li>Max Temp: {{dailyWeather.temperatureMax}} degF</li>
+					<li>Wind Speed: {{dailyWeather.windSpeed}} mph</li>
+					<li>Timestamp: {{dailyWeather.timestamp}} s</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
